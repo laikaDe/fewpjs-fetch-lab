5f620acfd0bbedc754a5340819faa24ccf42df05
Laika Deunert <laikadeunert@MacBook-Air.local>
2021-12-20 22:56:41 -0800

    commit tree 8bdfd2a4426efaf07e37b197f5f8b3d24e53f74e

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  // .then(json => console.log(json));
+  const books = JSON.parse(json);
+
 }
 
 function renderBooks(books) {
